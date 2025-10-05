13e865339d4d41a85fa23b6b9ca9bd98916864a7
developerlight <samsulmuhyidin@gmail.com>
2025-10-05 22:01:36 +0700

    feat register page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:date_field/date_field.dart';
 
 main() => runApp(MyApp());
 
@@ -28,6 +29,8 @@ class MyApp extends StatelessWidget {
 }
 
 class CobaLogin extends StatelessWidget {
+  const CobaLogin({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -94,15 +97,34 @@ class CobaLogin extends StatelessWidget {
                   style: TextStyle(fontSize: 18, color: Colors.white),
                 ),
               ),
-              SizedBox(height: 20),
-              Text(
-                'Forgot Password?',
-                style: TextStyle(color: Colors.red[900]),
-              ),
               SizedBox(height: 10),
-              Text(
-                'Don\'t have an account? Sign Up',
-                style: TextStyle(color: Colors.red[900]),
+
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ForgotPasswordPage(),
+                    ),
+                  );
+                },
+                child: Text(
+                  'Forgot Password?',
+                  style: TextStyle(color: Colors.red[900]),
+                ),
+              ),
+              // SizedBox(height: 10),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => RegisterPage()),
+                  );
+                },
+                child: Text(
+                  'Don\'t have an account? Sign Up',
+                  style: TextStyle(color: Colors.red[900]),
+                ),
               ),
               SizedBox(height: 10),
               Text('Or sign in with', style: TextStyle(color: Colors.grey)),
@@ -156,6 +178,183 @@ class CobaLogin extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Register Page', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.red[900],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(maxWidth: 500),
+              child: Register(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Register extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Text(
+              'Register Form',
+              style: TextStyle(
+                fontSize: 30,
+                fontStyle: FontStyle.normal,
+                fontWeight: FontWeight.bold,
+                color: Colors.red[900],
+              ),
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              decoration: InputDecoration(
+                suffixIcon: Icon(Icons.person),
+                labelText: 'First Name',
+                hintText: 'Enter your first name',
+                border: UnderlineInputBorder(
+
+                  borderSide: BorderSide(color: Colors.red),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              decoration: InputDecoration(
+                suffixIcon: Icon(Icons.person),
+                labelText: 'Last Name',
+                hintText: 'Enter your last name',
+                border: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                suffixIcon: Icon(Icons.email),
+                labelText: 'Email',
+                hintText: 'Enter your email',
+                helperText: 'We will never share your email with anyone else.',
+                border: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              keyboardType: TextInputType.phone,
+              decoration: InputDecoration(
+                suffixIcon: Icon(Icons.phone),
+                labelText: 'Phone Number',
+                hintText: 'Enter your phone number',
+                border: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                suffixIcon: Icon(Icons.password),
+                labelText: 'Password',
+                hintText: 'Enter your password',
+                border: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            DateTimeFormField(
+              decoration: InputDecoration(
+                labelText: 'Date of Birth',
+                hintText: 'Select your date of birth',
+                border: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red),
+                ),
+              ),
+              mode: DateTimeFieldPickerMode.date,
+              autovalidateMode: AutovalidateMode.always,
+              validator: (e) => (e?.day ?? 0) == 1 ? 'Please not the first day' : null,
+              onChanged: (DateTime? value) {
+                print(value);
+              },
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Handle register action
+              }, 
+              child: Text(
+                'Register',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                minimumSize: Size(double.infinity, 50),
+                backgroundColor: Colors.red[900],
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+              ),
+            ),
+            SizedBox(height: 10),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text(
+                'Already have an account? Sign In',
+                style: TextStyle(color: Colors.red[900]),
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'By registering, you agree to our Terms and Conditions.',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 12, color: Colors.grey),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '© 2025 My Flutter App',
+              style: TextStyle(fontSize: 12, color: Colors.grey),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ForgotPasswordPage extends StatelessWidget {
+  const ForgotPasswordPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(title: Text('Forgot Password')),
+      body: Center(child: Text('Forgot Password Page')),
     );
   }
 }
