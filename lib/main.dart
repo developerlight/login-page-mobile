c0f627609b4da546ca836cf9d9960e5fd68253fd
developerlight <samsulmuhyidin@gmail.com>
2025-10-09 22:28:44 +0700

    feat feature validator loginPage and registerPage

--- lib/main.dart
+++ lib/main.dart
@@ -28,8 +28,24 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class CobaLogin extends StatelessWidget {
+class CobaLogin extends StatefulWidget {
   const CobaLogin({super.key});
+
+  @override
+  State<CobaLogin> createState() => _CobaLogin();
+}
+
+class _CobaLogin extends State<CobaLogin> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,6 +55,8 @@ class CobaLogin extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
+          key: _formKey,
+          // autovalidateMode: AutovalidateMode.onUserInteraction,
           child: Column(
             children: [
               SizedBox(height: 12),
@@ -54,6 +72,7 @@ class CobaLogin extends StatelessWidget {
               ),
               SizedBox(height: 20),
               TextFormField(
+                controller: _usernameController,
                 decoration: InputDecoration(
                   prefixIcon: Icon(Icons.person),
                   labelText: 'Username',
@@ -63,11 +82,14 @@ class CobaLogin extends StatelessWidget {
                     borderSide: BorderSide(color: Colors.red),
                   ),
                 ),
+                // menampilkan pesan error jika field kosong
+                validator: (value) =>
+                    value!.isEmpty ? 'Please enter your username' : null,
               ),
               SizedBox(height: 10),
               TextFormField(
                 obscureText: true,
-
+                controller: _passwordController,
                 decoration: InputDecoration(
                   prefixIcon: Icon(Icons.lock),
                   labelText: 'Password',
@@ -77,12 +99,28 @@ class CobaLogin extends StatelessWidget {
                     borderSide: BorderSide(color: Colors.red),
                   ),
                 ),
+                validator: (value) =>
+                    value!.isEmpty ? 'Please enter your password' : null,
               ),
 
               SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => DashboardPage()));
+                  // print('Username: ${_usernameController.text}');
+                  // print('Password: ${_passwordController.text}');
+                  // Validate the form fields. If invalid, the TextFormField validators
+                  // will display error messages under each field automatically.
+                  if (_formKey.currentState!.validate()) {
+                    // If the form is valid, show a success snackbar (or proceed).
+                    ScaffoldMessenger.of(
+                      context,
+                    ).showSnackBar(SnackBar(content: Text('Login successful')));
+                    // Navigate to the dashboard or perform sign-in logic here.
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => DashboardPage()),
+                    );
+                  }
                 },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.red[900],
@@ -207,140 +245,177 @@ class RegisterPage extends StatelessWidget {
   }
 }
 
-class Register extends StatelessWidget {
+class Register extends StatefulWidget {
+  @override
+  State<Register> createState() => _Register();
+}
+
+class _Register extends State<Register> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Text(
-              'Register Form',
-              style: TextStyle(
-                fontSize: 30,
-                fontStyle: FontStyle.normal,
-                fontWeight: FontWeight.bold,
-                color: Colors.red[900],
-              ),
-            ),
-            SizedBox(height: 20),
-            TextFormField(
-              decoration: InputDecoration(
-                suffixIcon: Icon(Icons.person),
-                labelText: 'First Name',
-                hintText: 'Enter your first name',
-                border: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.red),
+      child: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text(
+                'Register Form',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red[900],
                 ),
               ),
-            ),
-            SizedBox(height: 10),
-            TextFormField(
-              decoration: InputDecoration(
-                suffixIcon: Icon(Icons.person),
-                labelText: 'Last Name',
-                hintText: 'Enter your last name',
-                border: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.red),
+              SizedBox(height: 20),
+              TextFormField(
+                controller: _firstNameController,
+                decoration: InputDecoration(
+                  suffixIcon: Icon(Icons.person),
+                  labelText: 'First Name',
+                  hintText: 'Enter your first name',
+                  border: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red),
+                  ),
+                ),
+                validator: (value) => value!.isEmpty ? 'Please enter your first name' : null,
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: _lastNameController,
+                decoration: InputDecoration(
+                  suffixIcon: Icon(Icons.person),
+                  labelText: 'Last Name',
+                  hintText: 'Enter your last name',
+                  border: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red),
+                  ),
+                ),
+                validator: (value) => value!.isEmpty ? 'Please enter your last name' : null,
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  suffixIcon: Icon(Icons.email),
+                  labelText: 'Email',
+                  hintText: 'Enter your email',
+                  helperText:
+                      'We will never share your email with anyone else.',
+                  border: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red),
+                  ),
+                ),
+                validator: (value) =>
+                    value!.isEmpty ? 'Please enter your email' : null,
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: _phoneController,
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(
+                  suffixIcon: Icon(Icons.phone),
+                  labelText: 'Phone Number',
+                  hintText: 'Enter your phone number',
+                  border: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red),
+                  ),
+                ),
+                validator: (value) => value!.isEmpty ? 'Please enter your phone number' : null,
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  suffixIcon: Icon(Icons.password),
+                  labelText: 'Password',
+                  hintText: 'Enter your password',
+                  border: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red),
+                  ),
+                ),
+                validator: (value) =>
+                    value!.isEmpty ? 'Please enter your password' : null,
+              ),
+              SizedBox(height: 10),
+              DateTimeFormField(
+                decoration: InputDecoration(
+                  labelText: 'Date of Birth',
+                  hintText: 'Select your date of birth',
+                  border: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red),
+                  ),
+                ),
+                mode: DateTimeFieldPickerMode.date,
+                autovalidateMode: AutovalidateMode.always,
+                validator: (e) =>
+                    (e?.day ?? 0) == 1 ? 'Please not the first day' : null,
+                onChanged: (DateTime? value) {
+                  print(value);
+                },
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Handle register action
+                  if (_formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of( 
+                      context,
+                    ).showSnackBar(
+                        SnackBar(content: Text('Registration successful'))
+                  );
+                  Navigator.pop(context);
+                  }
+                },
+                child: Text(
+                  'Register',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  minimumSize: Size(double.infinity, 50),
+                  backgroundColor: Colors.red[900],
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
                 ),
               ),
-            ),
-            SizedBox(height: 10),
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                suffixIcon: Icon(Icons.email),
-                labelText: 'Email',
-                hintText: 'Enter your email',
-                helperText: 'We will never share your email with anyone else.',
-                border: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.red),
+              SizedBox(height: 10),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  'Already have an account? Sign In',
+                  style: TextStyle(color: Colors.red[900]),
                 ),
               ),
-            ),
-            SizedBox(height: 10),
-            TextFormField(
-              keyboardType: TextInputType.phone,
-              decoration: InputDecoration(
-                suffixIcon: Icon(Icons.phone),
-                labelText: 'Phone Number',
-                hintText: 'Enter your phone number',
-                border: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.red),
-                ),
+              SizedBox(height: 10),
+              Text(
+                'By registering, you agree to our Terms and Conditions.',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 12, color: Colors.grey),
               ),
-            ),
-            SizedBox(height: 10),
-            TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                suffixIcon: Icon(Icons.password),
-                labelText: 'Password',
-                hintText: 'Enter your password',
-                border: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.red),
-                ),
+              SizedBox(height: 10),
+              Text(
+                '© 2025 My Flutter App',
+                style: TextStyle(fontSize: 12, color: Colors.grey),
               ),
-            ),
-            SizedBox(height: 10),
-            DateTimeFormField(
-              decoration: InputDecoration(
-                labelText: 'Date of Birth',
-                hintText: 'Select your date of birth',
-                border: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.red),
-                ),
-              ),
-              mode: DateTimeFieldPickerMode.date,
-              autovalidateMode: AutovalidateMode.always,
-              validator: (e) =>
-                  (e?.day ?? 0) == 1 ? 'Please not the first day' : null,
-              onChanged: (DateTime? value) {
-                print(value);
-              },
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Handle register action
-              },
-              child: Text(
-                'Register',
-                style: TextStyle(fontSize: 18, color: Colors.white),
-              ),
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                minimumSize: Size(double.infinity, 50),
-                backgroundColor: Colors.red[900],
-                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-              ),
-            ),
-            SizedBox(height: 10),
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text(
-                'Already have an account? Sign In',
-                style: TextStyle(color: Colors.red[900]),
-              ),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'By registering, you agree to our Terms and Conditions.',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 12, color: Colors.grey),
-            ),
-            SizedBox(height: 10),
-            Text(
-              '© 2025 My Flutter App',
-              style: TextStyle(fontSize: 12, color: Colors.grey),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -687,7 +762,7 @@ class DashboardPage extends StatelessWidget {
           'Welcome to the Dashboard!',
           style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
         ),
-      )
+      ),
     );
   }
 }
